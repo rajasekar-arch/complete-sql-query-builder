055b399d1773ff6518a4ea99162e6927a0a24c48
Rajasekar <erajasekargv@gmail.com>
2025-07-08 14:11:20 +0530

    complete-sql-query-builder: deploy commit

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/complete-sql-query-builder',
-  assetPrefix: '/complete-sql-query-builder/',
   images: {
     unoptimized: true,
   },
