f63794a76d63beae30a8d742a479777564d24d1b
Rajasekar <erajasekargv@gmail.com>
2025-07-08 15:32:30 +0530

    complete-sql-query-builder: deploy commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/complete-sql-query-builder',
+  assetPrefix: '/complete-sql-query-builder/',
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    // ...add other folders if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
