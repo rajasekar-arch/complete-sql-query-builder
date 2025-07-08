403613347768b78fb780a7359900373f48385482
Rajasekar <erajasekargv@gmail.com>
2025-07-08 15:46:27 +0530

    complete-sql-query-builder: css updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,6 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
     './components/*.{js,ts,jsx,tsx}',
   ],
   theme: {
