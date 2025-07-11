ae6c91ddb7f7169cef3f77a520a39f09733ea918
Rajasekar <erajasekargv@gmail.com>
2025-07-11 11:23:15 +0530

    complete-sql-query-builder: folder updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./src/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
