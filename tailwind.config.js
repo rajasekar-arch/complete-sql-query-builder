b6360a4a5fc68a713d5ea6690c9800304db83dec
Rajasekar <erajasekargv@gmail.com>
2025-07-11 11:49:01 +0530

    complete-sql-query-builder: folder updated]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
     // tailwind.config.js
     module.exports = {
       content: [
+        "./src/*.{js,ts,jsx,tsx}", // Include root src directory
         "./src/components/*.{js,ts,jsx,tsx}",
         "./src/app/**/*.{js,ts,jsx,tsx}", // Include app directory if using App Router
       ],
